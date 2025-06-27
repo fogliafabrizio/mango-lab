693ebb3346d1a4cfc427c8c11f557d61a682adb0
Fabrizio Foglia <fabryfoglia@me.com>
2025-06-27 16:14:57 +0200

    Fix application tests

--- backend/src/test/java/com/mango_lab/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/mango_lab/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.mango_lab.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
