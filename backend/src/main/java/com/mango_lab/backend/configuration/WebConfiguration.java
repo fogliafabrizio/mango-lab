a79e3a9e2873fe4a884d66f01596b1a905b250e5
Fabrizio Foglia <fabryfoglia@me.com>
2025-06-28 20:53:13 +0200

    fixing allowed origins

--- backend/src/main/java/com/mango_lab/backend/configuration/WebConfiguration.java
+++ backend/src/main/java/com/mango_lab/backend/configuration/WebConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Value("${frontend.domain}")
+    @Value("${management.endpoints.web.cors.allowed-origins}")
     private String frontendDomain;
 
     @Override
